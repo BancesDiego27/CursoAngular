980aea6807d7a3193fccccf7bbd98970007060b7
BancesDiego27 <diego.bances@galileo.edu>
2023-11-25 22:43:34 -0600

    fin seccion 3

---
+++ src/app/auth/register/register.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'register',
+  templateUrl: './register.component.html',
+  styleUrls: [ '../login/login.component.css' ]
+})
+export class RegisterComponent {
+
+}
